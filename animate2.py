85a8e0202e09cc97877d77380657545878bad413
ne.umnepatop <andrei.zelepugin@yandex.ru>
2024-06-21 12:27:10 +0300

    now viewing results

---
+++ animate2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('inverted_pendulum.csv', delimiter=',')
+
+states=data[:4]
+actions = data[-1]
+
+velocities = states[-1]
+fig, ax = plt.subplots()
+ax.set_xlabel('iter')
+ax.set_ylabel('V')
+ax.plot([i for i in range(len(velocities))], velocities)
+plt.savefig('inverted_pendulum.png')
