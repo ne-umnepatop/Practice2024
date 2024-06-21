e89c14c7da187c443ea2d9dd94ab46e768a7b096
ne.umnepatop <andrei.zelepugin@yandex.ru>
2024-06-21 17:08:32 +0300

    полный вывод графиков

--- animate2.py
+++ animate2.py
@@ -3,14 +3,25 @@ import matplotlib.pyplot as plt
 
 data = np.genfromtxt('inverted_pendulum.csv', delimiter=',')
 
-states = data[:3]
-actions = data[-1]
-velocities = states[-1]
+states     = data[:4]
+actions    = data[-1]
+poses      = states[0]
+angles     = states[1]
+cart_vs    = states[2]
+velocities = states[3]
+iters = [i for i in range(len(velocities))]
 
-fig, ax = plt.subplots()
-ax.set_xlabel('iter')
-ax.set_ylabel('V')
-ax.plot([i for i in range(len(velocities))], velocities, label='ang_vel')
-ax.plot([i for i in range(len(velocities))], actions, label='action')
-plt.legend()
+fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1)
+
+ax0.set_xlabel('iter')
+ax0.plot(iters, velocities, label='ang_vel')
+ax0.plot(iters, actions, label='action')
+ax0.plot(iters, angles, label='angle')
+ax0.legend()
+
+ax1.set_xlabel('iter')
+ax1.plot(iters, poses, label='poses')
+ax1.plot(iters, cart_vs, label='cart_vs')
+ax1.legend()
+
 plt.savefig('inverted_pendulum.png')
